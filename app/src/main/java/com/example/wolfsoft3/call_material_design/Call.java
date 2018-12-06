19cdc80808d60ed0fc2737c6ee8b089112089760
Rushabh Patel <wsdesignuiux@gmail.com>
2018-12-06 15:43:58 +0530

    Initial commit

---
+++ app/src/main/java/com/example/wolfsoft3/call_material_design/Call.java
@@ -0,0 +1,13 @@
+package com.example.wolfsoft3.call_material_design;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Call extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.call);
+    }
+}
